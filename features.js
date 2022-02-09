2511140ce21872f4de406f623691b85dd0cd10e9
stefanbielmeier <stefanbielmeier@icloud.com>
2022-02-09 11:11:58 +0100

    service worker test

--- features.js
+++ features.js
@@ -1,8 +1,26 @@
-const logURL = (requestDetails) => {
-    console.log("Loading: " + requestDetails.url);
-  }
-  
-  browser.webRequest.onBeforeRequest.addListener(
-    logURL,
-    {urls: ["<all_urls>"]}
-);
+
+
+
+//Register the service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+  // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
+const defineFeatures = () => {
+
+  self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting());
+  });
+
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+  });
+
+}
+
