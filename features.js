c3d792fdba83b0a904a69401ae44eb14980df1d1
stefanbielmeier <stefanbielmeier@icloud.com>
2022-02-09 11:25:55 +0100

    update listeners

--- features.js
+++ features.js
@@ -14,13 +14,12 @@ if ('serviceWorker' in navigator) {
 
 self.addEventListener('install', (event) => {
   event.waitUntil(self.skipWaiting());
+  console.log("install "+event)
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
+  console.log('activate '+event)
 });
 
-self.addEventListener('fetch', (event) => {
-  console.log(event)
-})
-
+console.log('End of script')
