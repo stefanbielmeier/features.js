8dade11db47f3b3a4939977b68bd6b8dcfdae6c1
stefanbielmeier <stefanbielmeier@icloud.com>
2022-02-09 17:49:29 +0100

    added backend data sending

--- features.js
+++ features.js
@@ -1,4 +1,23 @@
+const sendToBackend = async (request) => {
+  fetch('localhost:3000/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(request),
+  })
+  .then(response => response.json())
+  .then(data => {
+  console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+
 if(!this.document) {
+
   self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
     console.log("install " + event);
@@ -9,23 +28,31 @@ if(!this.document) {
     console.log("activate " + event);
   });
   
-  self.addEventListener("fetch", function (event) {
+  self.addEventListener("fetch", async (event) => {
     
     //1) Get each outgoing HTTP Request to Backend (incl. URL) and point in time
+    //TODO: Add more information (e.g. headers if possible)
+    //https://stackoverflow.com/questions/57583086/service-worker-not-getting-body-from-request
+
     timestamp = new Date()
     url = event.request.url
     unixTimestamp = timestamp.getTime()
     method = event.request.method
 
     if (url.includes("heroku")){
-      console.log(timestamp, unixTimestamp, method, url)
+      request = {
+        timestamp: timestamp,
+        unixTimestamp: unixTimestamp,
+        method: method,
+        url: url
+      }
+      
+      console.log(request)
+
+      //2) Send request information to backend (later: for specific trackerID
+      await sendToBackend(request)
     }
   
-    //TODO: Add more information (e.g. headers if possible)
-    //https://stackoverflow.com/questions/57583086/service-worker-not-getting-body-from-request
-
-    //2) Record all requests and send them to backend (later: for specific trackerID)
-
     //3) Count requests with same url endpoint, method, and timestamp in same day
 
     //4) Put it in a graph: x-axis day (date), y-axis: #requests
@@ -34,10 +61,10 @@ if(!this.document) {
 
     //6) Login etc.
 
+  });  
 
-  });
-  
-  console.log("End of service worker");  
+
+
 
 } else {
   //Register the service worker
