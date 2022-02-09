529aea1164d1cf99177ef16c84c02deaaabbf4f1
stefanbielmeier <stefanbielmeier@icloud.com>
2022-02-09 11:32:16 +0100

    add url

--- features.js
+++ features.js
@@ -3,7 +3,7 @@
 
 //Register the service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js').then((registration) => {
+  navigator.serviceWorker.register('https://gitfront.io/r/user-6005028/c158b63770de1bc3fe53cae285a592578a94a8e3/features.js/raw/service-worker.js').then((registration) => {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ', registration.scope);
   }).catch(function(err) {
