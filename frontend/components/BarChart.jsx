8dfd9ff87f6a66ab0de4e24eab24a04c8f0fe43c
stefanbielmeier <stefanbielmeier@icloud.com>
2022-02-10 14:39:43 +0100

    made bar chart nice

--- frontend/components/BarChart.jsx
+++ frontend/components/BarChart.jsx
@@ -7,27 +7,40 @@ import {
   HorizontalGridLines,
   VerticalGridLines,
   VerticalBarSeries,
-  LabelSeries,
 } from "react-vis";
 
 const MSEC_DAILY = 86400000;
 
-const timestamp = new Date("Feb 01 2022").getTime();
+const timestamp = new Date("Feb 01 2022").getTime()
 
 const data = [
     { x: timestamp + MSEC_DAILY, y: 3 },
-    { x: timestamp + MSEC_DAILY * 2, y: 24 },
+    { x: timestamp + MSEC_DAILY * 3, y: 24 },
+    { x: timestamp + MSEC_DAILY * 5, y: 30 },
+    { x: timestamp + MSEC_DAILY * 8, y: 15 },
 ]
+
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+const dataArr = data.map((d)=> {
+    let date = new Date(d.x)
+    let displDate = (monthNames[date.getMonth()]) + " " + date.getDate()  
+    return {x: displDate, y: parseFloat(d.y)}
+})  
+
+console.log(dataArr)
 
 export default function BarChart() {
 
   return (
-      <XYPlot xType="time" width={450} height={300}>
+      <XYPlot xType="ordinal" width={450} height={300}>
         <HorizontalGridLines />
         <VerticalGridLines />
         <XAxis title="Day" />
         <YAxis title="Number of User Actions" />
-        <VerticalBarSeries data={data} />
+        <VerticalBarSeries data={dataArr} />
     </XYPlot>
   );
 
