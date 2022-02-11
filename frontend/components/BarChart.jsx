eab41a5cc7d89d7366fc803ae671db77a61d96d6
stefanbielmeier <stefanbielmeier@icloud.com>
2022-02-11 13:52:26 +0100

    working fetching of count

--- frontend/components/BarChart.jsx
+++ frontend/components/BarChart.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+import { createClient } from '@supabase/supabase-js'
 
 import {
   XYPlot,
@@ -13,24 +15,78 @@ const MSEC_DAILY = 86400000;
 
 const timestamp = new Date("Feb 01 2022").getTime()
 
-const data = [
-    { x: timestamp + MSEC_DAILY, y: 3 },
-    { x: timestamp + MSEC_DAILY * 3, y: 24 },
-    { x: timestamp + MSEC_DAILY * 5, y: 30 },
-    { x: timestamp + MSEC_DAILY * 8, y: 15 },
-]
-
 const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 ];
 
-const dataArr = data.map((d)=> {
-    let date = new Date(d.x)
-    let displDate = (monthNames[date.getMonth()]) + " " + date.getDate()  
-    return {x: displDate, y: parseFloat(d.y)}
-})  
+const getDay = (timestamp) => {
+    const date = new Date(timestamp)
+    return monthNames[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear()
+}
+
+const mapData = (rawData) => {
+    /*
+    gets: Array of Objects ({id,  unix_timestamp in UTC})
+
+    Returns: array of objects with {x: timestamp as a legible string, e.g. Feb 2 2022, y: count}
+
+    */
+    console.log(rawData)
+    
+    var counter = new Map()
+
+    for (let request of rawData) {
+        const date = getDay(request.unix_timestamp)
+
+        if (counter.has(date)) {
+            var currentVal = counter.get(date)
+            counter.set(date, {x: currentVal.x, y: currentVal.y + 1})
+        }
+        if (!counter.has(date)) {
+            const newDay = {x: date, y: 1}
+            console.log("not in ocunter")
+            counter.set(date, newDay)
+        }
+    }
+    const output = Array.from(counter.values());
+    console.log("putput", output)
+    return output
+}
+
+const SUPABASE_URL = "https://gvazgvbsrhzxpcadfzcb.supabase.co"
+
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabase = createClient(SUPABASE_URL, supabaseKey)
+
+
+
+const fetchFromBackend = async (setData) => {
+    try {
+        let { data, error, status } = await supabase
+            .from('requests')
+            .select('*')
+    
+        if (error && status !== 406) {
+          throw error
+        }
+  
+        if (data) {
+            console.log("log", data)
+            const mappedData = mapData(data)
+            setData(mappedData)
+        }
+    } catch (error) {
+        alert(error.message)
+    }
+}
 
 export default function BarChart() {
+
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        fetchFromBackend(setData)
+    },[])
 
   return (
       <XYPlot xType="ordinal" width={450} height={300}>
@@ -38,7 +94,7 @@ export default function BarChart() {
         <VerticalGridLines />
         <XAxis title="Day" />
         <YAxis title="Number of User Actions" />
-        <VerticalBarSeries data={dataArr} />
+        <VerticalBarSeries data={data} />
     </XYPlot>
   );
 
