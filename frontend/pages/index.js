fa13867d750a6b98d4fc6e76765f583d9820f91f
stefanbielmeier <stefanbielmeier@icloud.com>
2022-02-13 18:14:07 +0100

    tried adding download link and public folder

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
 import Head from "next/head";
-import SourceSelector from '../components/SourceSelector';
+import SourceSelector from "../components/SourceSelector";
 
 export default function Home() {
-
   return (
     <div className="container">
       <Head>
@@ -14,7 +13,15 @@ export default function Home() {
       <main>
         <h1 className="title font-bold">Welcome to Features.js!</h1>
 
-        <p className="description">See which features users use based on your API requests</p>
+        <p className="description">
+          See which features users use based on your API requests
+        </p>
+        <p className='text'>Installation: 1) Download </p> <a download="features.js" href='https://raw.githubusercontent.com/stefanbielmeier/features.js/master/script/features.js?token=GHSAT0AAAAAABNVU774QGAIARODWAS2TF7QYQSOZ6Q'>features.js</a>
+        <p className='text'>
+          2) Put it in the public folder of your React App 
+        </p>
+        <p className='text'>3) Start tracking
+          below</p>
 
         <SourceSelector />
       </main>
